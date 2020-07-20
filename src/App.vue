ad85dc2f2e05bf8eacf2efe5a75fd95b83f3e15f
ZhangZisu <admin@zhangzisu.cn>
2020-07-20 21:20:24 +0800

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <v-app-bar app>
+      <v-btn icon tile>
+        <v-avatar tile size="32">
+          <v-img src="@/assets/logo.svg"/>
+        </v-avatar>
+      </v-btn>
+      <div class="ml-2">
+        <div class="font-weight-bold">ZhangZisu.CN</div>
+        <div class="text-overline" style="line-height: unset;">Proxy Services</div>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-main>
+    <div id="z-bg">
+      <v-img src="https://proxy.la.workers.dev/pximg/img-original/img/2019/04/09/21/13/01/74125498_p0.png" contain height="100%"/>
     </div>
-    <router-view/>
-  </div>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  //
 }
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+#z-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,118 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-row justify="center">
+    <v-col cols="12" lg="6">
+      <v-card>
+        <v-card-title>Home</v-card-title>
+        <v-divider/>
+        <v-card-subtitle>RawGit</v-card-subtitle>
+        <v-row no-gutters class="mr-3">
+          <v-col>
+            <v-text-field v-model="ipt1" placeholder="https://raw.githubusercontent.com/..." class="pa-4 pt-0" clearable/>
+          </v-col>
+          <v-col cols="auto" @click="gn1">
+            <v-btn :disabled="!vld1">Proxy!</v-btn>
+          </v-col>
+        </v-row>
+        <v-divider/>
+        <v-card-subtitle>Pixiv Image</v-card-subtitle>
+        <v-row no-gutters class="mr-3">
+          <v-col>
+            <v-text-field v-model="ipt2" placeholder="https://i.pximg.net/..." class="pa-4 pt-0" clearable/>
+          </v-col>
+          <v-col cols="auto" @click="gn2">
+            <v-btn :disabled="!vld2">Proxy!</v-btn>
+          </v-col>
+        </v-row>
+        <v-divider/>
+        <v-card-subtitle>Github Release</v-card-subtitle>
+        <v-row no-gutters class="mr-3">
+          <v-col>
+            <v-text-field v-model="ipt3" placeholder="https://github.com/.../.../releases/download/..." class="pa-4 pt-0" clearable/>
+          </v-col>
+          <v-col cols="auto" @click="gn3">
+            <v-btn :disabled="!vld3">Proxy!</v-btn>
+          </v-col>
+        </v-row>
+        <v-dialog v-model="dialog" max-width="480">
+          <v-card>
+            <v-card-title>
+              Proxy link generated!
+            </v-card-title>
+            <v-divider/>
+            <v-card-text>
+              <div style="overflow-x: scroll" class="mt-4">
+                <code style="white-space: nowrap">{{ generated }}</code>
+              </div>
+            </v-card-text>
+            <v-divider/>
+            <v-card-actions>
+              <v-spacer/>
+              <v-btn color="green">
+                <v-icon left>mdi-content-copy</v-icon>
+                Copy
+              </v-btn>
+              <v-btn color="blue" :href="generated" target="_blank">
+                <v-icon left>mdi-open-in-new</v-icon>
+                Open
+              </v-btn>
+              <v-btn color="error" @click="dialog = false">
+                <v-icon left>mdi-close</v-icon>
+                Close
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+@Component
+export default class Home extends Vue {
+  ipt1 = ''
+  ipt2 = ''
+  ipt3 = ''
+  dialog = false
+
+  generated = ''
+
+  get vld1 () {
+    const r = /https?:\/\/raw.githubusercontent.com(.+)/.exec(this.ipt1)
+    return r && r[1]
+  }
+
+  get vld2 () {
+    const r = /https?:\/\/i.pximg.net(.+)/.exec(this.ipt2)
+    return r && r[1]
+  }
+
+  get vld3 () {
+    const r = /https?:\/\/github.com(.+)/.exec(this.ipt3)
+    return r && r[1]
+  }
+
+  gn1 () {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const path = /https?:\/\/raw.githubusercontent.com(.+)/.exec(this.ipt1)![1]
+    this.generated = 'https://proxy.la.workers.dev/rawgit' + path
+    this.dialog = true
+  }
+
+  gn2 () {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const path = /https?:\/\/i.pximg.net(.+)/.exec(this.ipt2)![1]
+    this.generated = 'https://proxy.la.workers.dev/pximg' + path
+    this.dialog = true
+  }
+
+  gn3 () {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const path = /https?:\/\/github.com(.+)/.exec(this.ipt3)![1]
+    this.generated = 'https://proxy.la.workers.dev/github' + path
+    this.dialog = true
   }
 }
 </script>
